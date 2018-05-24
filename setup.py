9020587af24a4308223a91ce2d1f4944ec6a5eca
Matthew D. Scholefield <matthew331199@gmail.com>
2018-05-23 22:02:05 -0500

    Increment version to 0.1.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='msk',
-    version='0.1.0',  # Also update in msu/__init__.py
+    version='0.1.2',  # Also update in msu/__init__.py
     packages=['msk'],
     install_requires=['GitPython', 'typing', 'msm', 'pygithub', 'future'],
     url='https://github.com/MycroftAI/mycroft-skills-kit',
