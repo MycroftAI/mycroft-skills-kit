bad1abb7c52074f95bfb4b5fa7a3072a39c2ffef
Matthew D. Scholefield <matthew331199@gmail.com>
2018-05-29 16:47:49 -0500

    Fix bug with asking for input twice

--- msk/lazy.py
+++ msk/lazy.py
@@ -3,11 +3,14 @@ from functools import wraps
 
 class Lazy:
     """Lazy attribute across all instances"""
+    initial_val = []
+
     def __init__(self, func):
         wraps(func)(self)
         self.func = func
-        self.return_val = None
+        self.return_val = self.initial_val
 
     def __get__(self, inst, inst_cls):
-        self.return_val = self.return_val or self.func(inst)
+        if self.return_val is self.initial_val:
+            self.return_val = self.func(inst)
         return self.return_val
