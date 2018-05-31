61e652c4726cc8ead25126fea03831c4d817b802
Matthew D. Scholefield <matthew331199@gmail.com>
2018-05-30 20:17:46 -0500

    Increment version to 0.3.0

--- msk/__init__.py
+++ msk/__init__.py
@@ -19,4 +19,4 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-__version__ = '0.1.3'  # Also update in setup.py
+__version__ = '0.3.0'  # Also update in setup.py
