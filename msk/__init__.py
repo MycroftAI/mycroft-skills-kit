4c8ca15e481639651f7210f911544135044d4832
Matthew D. Scholefield <matthew331199@gmail.com>
2018-06-05 16:27:58 -0500

    Increment version to 0.3.6

--- msk/__init__.py
+++ msk/__init__.py
@@ -19,4 +19,4 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-__version__ = '0.3.5'  # Also update in setup.py
+__version__ = '0.3.6'  # Also update in setup.py
